82978d435b9475cbbfb832631706b1df15a2a894
Luis Castañeda <luis.castaneda@sicuentame.com>
2025-10-24 19:44:49 -0600

    Add tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from "./app";
 
 const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || "http://localhost";
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on ${HOST}:${PORT}`);
 });
